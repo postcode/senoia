3b7dccea958f80b328e9b7d4e1db642719f46503
andyhull <andyhull3@gmail.com>
2015-05-19 14:45:46 -0400

    added mandrill setup.

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.mandrillapp.com',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['MANDRILL_USERNAME'],
+  :password       => ENV['MANDRILL_PASSWORD'],
+  :domain         => 'senioa.com',
+  :enable_starttls_auto => true
+}
