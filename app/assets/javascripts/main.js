88d8d44fb9ce0400074d427b2b65ecc661f0a4d7
andyhull <andyhull3@gmail.com>
2015-05-21 06:36:52 -0400

    changes to the plan archive page

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,7 +1,7 @@
 $(function() {
-  $('.dateSelect').fdatepicker({
-    format: 'mm/dd/yyyy'
-  }); 
+  // $('.dateSelect').fdatepicker({
+  //   format: 'mm/dd/yyyy'
+  // }); 
 
   $('#all_plans tbody tr').css('cursor', 'pointer')
   $('#all_plans tbody tr').click(function() {
