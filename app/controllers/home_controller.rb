717374689ecf842485f6032071eba0dd3a9f333f
andyhull <andyhull3@gmail.com>
2015-06-26 17:18:08 -0400

    fixing the calendar on the home page.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,7 @@
 class HomeController < ApplicationController
   def index
     @plans = Plan.all
+    @operation_period = OperationPeriod.all
     respond_to do |format|
       format.html
       format.json { render json: @plans }
