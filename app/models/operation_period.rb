d8205bb497082fafb468a600467306dbdf08efd5
Ville <sundberg.ville@gmail.com>
2015-08-25 19:59:02 +0200

    Parse operation period start and date times in the model

--- app/models/operation_period.rb
+++ app/models/operation_period.rb
@@ -23,4 +23,25 @@ class OperationPeriod < ActiveRecord::Base
 
   accepts_nested_attributes_for :first_aid_stations, :mobile_teams, :transports, :dispatchs
 
+  validates :start_date, presence: true
+  validates :end_date, presence: true
+  
+  DEFAULT_DATETIME_FORMAT = "%m/%d/%Y %H:%M %p"
+  
+  def start_date=(new_start_date)
+    if new_start_date =~ /[0-9]{2}\/[0-9]{2}\/[0-9]{4}/
+      super(DateTime.strptime(new_start_date, DEFAULT_DATETIME_FORMAT))
+    else
+      super
+    end
+  end
+
+  def end_date=(new_end_date)
+    if new_end_date =~ /[0-9]{2}\/[0-9]{2}\/[0-9]{4}/
+      super(DateTime.strptime(new_end_date, DEFAULT_DATETIME_FORMAT))
+    else
+      super
+    end
+  end
+  
 end
