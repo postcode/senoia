ab7ec18cd97771c52d6b19b180e0e73ed269d2bc
Ville <sundberg.ville@gmail.com>
2015-08-26 09:59:50 +0200

    Added skeleton of 'new comment' notification

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,5 +1,23 @@
 class NotificationMailer < ActionMailer::Base
 
+  default from: "Senoia <senoia@senoia.com>"
   
+  def new_comment_notification(options = { recipient: nil, comment: nil })
+
+    @comment = options[:comment]
+    @commenter = @comment.user
+    @commentable = @comment.commentable
+
+    if @commentable.is_a? Plan
+      @plan = @commentable
+    elsif @commentable.is_a? OperationPeriod
+      @plan = @commentable.plan
+    else
+      raise ArgumentError.new("Don't now how to notify about this kind of comment: #{@comment.inspect}")
+    end
+    
+    mail(to: options[:recipient].email,
+         subject: "Someone commented on your medical plan")
+  end
   
 end
