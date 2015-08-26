4f66b496c2a48f31b8bb0f61e1aa292747c6bf94
Ville <sundberg.ville@gmail.com>
2015-08-26 14:30:16 +0200

    Add spec for adding comments and notifying users

--- spec/controllers/plans_controller_spec.rb
+++ spec/controllers/plans_controller_spec.rb
@@ -53,4 +53,36 @@ RSpec.describe PlansController do
       expect(@plane).to be_an_instance_of Plan
     end
   end
+
+  describe "#add_comment" do
+
+    let(:plan) { FactoryGirl.create(:plan) }
+    let(:message_delivery) { instance_double(ActionMailer::MessageDelivery) }
+    
+    context "when logged in as admin" do
+
+      login_admin
+
+      it "creates a comment" do
+        post :add_comment, { "comment_text" => Faker::Lorem.paragraph, "element_id" => "contact_comment_text", "id" => plan.id, "format" => "js" }
+        expect(response).to be_success
+        expect(assigns(:comment)).to be_an_instance_of Comment
+      end
+
+      let(:notification_recipient) { FactoryGirl.create(:user) }
+
+      it "sends notifications" do
+
+        plan.users << notification_recipient
+        
+        expect(NotificationMailer).to receive(:new_comment_notification)
+          .with({ recipient: notification_recipient, comment: kind_of(Comment) })
+          .and_return(message_delivery)
+        expect(message_delivery).to receive(:deliver_later)
+        
+        post :add_comment, { "comment_text" => Faker::Lorem.paragraph, "element_id" => "contact_comment_text", "id" => plan.id, "format" => "js" }
+      end
+      
+    end
+  end
 end
