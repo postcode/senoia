7c3ed38171775955ca5c6a0d088f227f728ec53e
Ville <sundberg.ville@gmail.com>
2015-08-28 21:28:53 +0200

    Add authorization to Replies#create

--- app/controllers/replies_controller.rb
+++ app/controllers/replies_controller.rb
@@ -2,6 +2,8 @@ class RepliesController < ApplicationController
 
   def create
     @comment = Comment.find(params[:comment_id])
+    authorize! :manage, @comment.commentable
+    
     @reply = Comment.create(reply_params.merge(commentable: @comment.commentable, user: current_user))
     @reply.move_to_child_of(@comment)
     redirect_to comments_url
