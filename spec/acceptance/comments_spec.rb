5f56885cdc3d6477144eab931563eeeec4b8c553
Ville <sundberg.ville@gmail.com>
2015-08-28 19:30:16 +0200

    Add dashboard specs

--- spec/acceptance/comments_spec.rb
+++ spec/acceptance/comments_spec.rb
@@ -10,6 +10,7 @@ feature "Comments" do
     background do
       @comment = create(:comment)
       @plan_with_outstanding_comment = create(:plan, comments: [ @comment ])
+      @reply_body = Faker::Lorem.paragraph
       
       sign_in(admin)
     end
@@ -21,17 +22,19 @@ feature "Comments" do
       end
       
       scenario "views comments" do
-        pending
         expect(page).to have_content(@plan_with_outstanding_comment.name)
         expect(page).to have_content(@comment.body)
       end
 
       scenario "resolves a comment" do
-        pending
+        click_on "RESOLVE"
+        expect(page).to_not have_content @comment.body
       end
       
       scenario "replies to a comment" do
-        pending
+        find("textarea").set @reply_body
+        click_on "REPLY"
+        expect(page).to have_content @reply_body
       end
 
     end
@@ -42,6 +45,10 @@ feature "Comments" do
         plan.comments << (@comment_on_event_type = create(:comment_on_event_type))
         @new_comment_body = Faker::Lorem.paragraph
         visit "/plans/#{plan.id}"
+      end
+
+      scenario "views a comment" do
+        expect(page).to have_content @comment_on_event_type.body
       end
       
       scenario "posts a comment" do
