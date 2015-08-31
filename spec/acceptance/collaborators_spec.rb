7c46601218615ff676c346da757edf93a53957b8
Ville <sundberg.ville@gmail.com>
2015-08-31 17:17:22 +0200

    Add spec for inviting new user and accepting invitation

--- spec/acceptance/collaborators_spec.rb
+++ spec/acceptance/collaborators_spec.rb
@@ -4,12 +4,14 @@ feature "Collaborators" do
 
   let(:admin) { FactoryGirl.create(:admin) }
   let(:plan) { FactoryGirl.create(:plan, workflow_state: "awaiting_review") }
+  let(:invite_email_address) { Faker::Internet.email }
 
   context "Admin", js: true do
     
     background do
       @collaborator = plan.plan_users.create(user: FactoryGirl.create(:user), role: "view").user
       @user = FactoryGirl.create(:user)
+
       sign_in(admin)
       visit "/plans/#{plan.id}"
     end
@@ -37,7 +39,46 @@ feature "Collaborators" do
 
       expect(find("fieldset", text: "COLLABORATORS")).to_not have_content(@collaborator.email)
     end
-    
+
+    scenario "invites a collaborator" do
+
+      click_on "ADD USERS"
+
+      sleep 1
+      find("#invitation-email").set(invite_email_address)
+      click_on "Invite"
+
+      expect(page).to have_content("Invite sent")
+      sign_out
+
+      open_email(invite_email_address)
+      click_email_link_matching(/sign_up/)
+
+      reset_mailer
+
+      password = Faker::Internet.password
+      fill_in "Email", with: invite_email_address
+      fill_in "user_password", with: password
+      fill_in "user_password_confirmation", with: password
+      click_on "Sign Up"
+
+      expect(page).to have_content("confirmation link")
+
+      open_email(invite_email_address)
+      click_email_link_matching(/confirm/)
+
+      visit "/users/sign_in"
+
+      fill_in "Email", with: invite_email_address
+      fill_in "Password", with: password
+
+      within ".form-actions" do
+        click_on "Login"
+      end
+
+      visit "/plans/#{plan.id}"
+      expect(find("fieldset", text: "COLLABORATORS")).to have_content(invite_email_address)
+    end
   end
 
 end
