4d8de69fd0062d69c5e61b0a90a2676c2b88a0aa
Ville <sundberg.ville@gmail.com>
2015-09-03 21:25:14 +0200

    Translate old email notifications to new system

--- app/mailers/notification_mailer.rb
+++ app/mailers/notification_mailer.rb
@@ -1,55 +1,16 @@
 class NotificationMailer < ActionMailer::Base
 
   default from: "Senoia <senoia@senoia.com>"
-  
-  def new_comment_notification(options = { recipient: nil, comment: nil })
 
-    @comment = options[:comment]
-    @commenter = @comment.user
-    @commentable = @comment.commentable
-
-    if @commentable.is_a? Plan
-      @plan = @commentable
-    elsif @commentable.is_a? OperationPeriod
-      @plan = @commentable.plan
-    else
-      raise ArgumentError.new("Don't now how to notify about this kind of comment: #{@comment.inspect}")
-    end
+  def notification(options = { notification: nil })
+    @notification = options[:notification]
+    @recipient = @notification.owner
+    instance_variable_set("@" + @notification.subject_type.underscore, @notification.subject)
     
-    mail(to: options[:recipient].email,
-         subject: "#{@commenter} commented on your medical plan")
+    mail(to: @recipient.email,
+         subject: t(@notification.translation_key,
+                    scope: "notification_mailer.subject",
+                    subject: @notification.subject))
   end
 
-  def plan_accepted_notification(options = { recipient: nil, plan: nil })
-
-    @plan = options[:plan]
-
-    mail(to: options[:recipient].email,
-         subject: "Your medical plan has been approved")
-  end
-
-  def plan_rejected_notification(options = { recipient: nil, plan: nil })
-
-    @plan = options[:plan]
-
-    mail(to: options[:recipient].email,
-         subject: "Your medical plan has been rejected")
-  end
-
-  def plan_submitted_notification(options = { recipient: nil, plan: nil })
-
-    @plan = options[:plan]
-
-    mail(to: options[:recipient].email,
-         subject: "A new medical plan has been submitted")
-  end
-
-  def plan_revision_requested_notification(options = { recipient: nil, plan: nil })
-    
-    @plan = options[:plan]
-
-    mail(to: options[:recipient].email,
-         subject: "Medical plan #{@plan.name} needs revision")
-  end
-  
 end
