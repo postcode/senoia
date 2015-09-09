1f9ccf3da84f197705b7dfc7badd9bee445646b5
Ville <sundberg.ville@gmail.com>
2015-09-09 17:46:45 +0200

    Allow server to boot without AWS credentials

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -8,21 +8,28 @@ if Rails.env.test?
   end
 
 else
-  
-  CarrierWave.configure do |config|
 
-    config.fog_credentials = {
-      provider: "AWS",
-      aws_access_key_id: Rails.application.secrets["aws_access_key_id"],
-      aws_secret_access_key: Rails.application.secrets["aws_secret_access_key"],
-      region: ENV["s3_region"]
-    }
+  if Rails.application.secrets["aws_access_key_id"]
+    
+    CarrierWave.configure do |config|
 
-    config.fog_directory = ENV["s3_bucket"]
-    config.fog_attributes = { cache_control: "public, max-age=315576000" }
+      config.fog_credentials = {
+        provider: "AWS",
+        aws_access_key_id: Rails.application.secrets["aws_access_key_id"],
+        aws_secret_access_key: Rails.application.secrets["aws_secret_access_key"],
+        region: ENV["s3_region"]
+      }
 
-    config.cache_dir = "#{Rails.root}/tmp/uploads"
+      config.fog_directory = ENV["s3_bucket"]
+      config.fog_attributes = { cache_control: "public, max-age=315576000" }
+
+      config.cache_dir = "#{Rails.root}/tmp/uploads"
+    end
+
+  else
+
+    puts "Warning: AWS credentials not configured. File uploads won't work."
+    
   end
-  
 end
 
