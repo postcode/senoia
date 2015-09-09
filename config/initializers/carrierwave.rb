0126438b81ba57fc17baefab111a88f09b9268d1
Ville <sundberg.ville@gmail.com>
2015-09-09 17:12:20 +0200

    CarrierWave setup for tests

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -1,15 +1,28 @@
 require 'carrierwave/processing/mime_types'
 
-CarrierWave.configure do |config|
-  config.fog_credentials = {
-    provider: "AWS",
-    aws_access_key_id: Rails.application.secrets["aws_access_key_id"],
-    aws_secret_access_key: Rails.application.secrets["aws_secret_access_key"],
-    region: ENV["CARRIERWAVE_REGION"]
-  }
+if Rails.env.test? && Rails.application.secrets["aws_access_key_id"].blank?
 
-  config.fog_directory = ENV["CARRIERWAVE_S3_BUCKET"]
-  config.fog_attributes = { cache_control: "public, max-age=315576000" }
+  CarrierWave.configure do |config|
+    config.storage = :file
+    config.enable_processing = false
+  end
 
-  config.cache_dir = "#{Rails.root}/tmp/uploads"
+else
+  
+  CarrierWave.configure do |config|
+
+    config.fog_credentials = {
+      provider: "AWS",
+      aws_access_key_id: Rails.application.secrets["aws_access_key_id"],
+      aws_secret_access_key: Rails.application.secrets["aws_secret_access_key"],
+      region: ENV["CARRIERWAVE_REGION"]
+    }
+
+    config.fog_directory = ENV["CARRIERWAVE_S3_BUCKET"]
+    config.fog_attributes = { cache_control: "public, max-age=315576000" }
+
+    config.cache_dir = "#{Rails.root}/tmp/uploads"
+  end
+  
 end
+
