565d1dc46a00f0a6b05153a0893456d34a82096f
Ville <sundberg.ville@gmail.com>
2015-09-23 13:40:12 +0200

    Fix view permissions for guests and viewers

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,11 +5,13 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.has_role? :admin
       can :manage, :all
-    else
-      can :manage, Plan, :creator_id => user.id
-      can :manage, Plan, :plan_users => { role: "edit", user_id: user.id }
+    elsif user.id?
+      can :manage, Plan, creator_id: user.id
+      can :manage, Plan, plan_users: { role: "edit", user_id: user.id }
+      can :read, Plan, plan_users: { role: "view", user_id: user.id }
       can :manage, ProviderConfirmation, provider: { contact_users: { id: user.id }}
-      can :read, :all
+    else
+      can :read, Plan, { workflow_state: "accepted" }
     end
   end
 end
