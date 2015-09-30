79b8d3b2d3eb63387cd9d09983fc1dfe8e4b74ca
Ville <sundberg.ville@gmail.com>
2015-09-30 12:06:41 +0200

    Clean up UsersController

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,161 +1,68 @@
 class UsersController < ApplicationController
-# from http://www.tonyamoyal.com/2010/09/29/rails-authentication-with-devise-and-cancan-part-2-restful-resources-for-administrators/
-  before_filter :get_user, :only => [:index,:new,:edit]
-  before_filter :authenticate_user!, :only => [:new, :edit, :show, :update, :create, :index]
-  before_filter :get_particular_user
-  load_and_authorize_resource :only => [:show,:new,:destroy,:edit,:update, :index]
-  before_filter :verify_is_admin
- 
-  # GET /users
-  # GET /users.xml                                                
-  # GET /users.json                                       HTML and AJAX
-  #-----------------------------------------------------------------------
+
+  before_filter :authenticate_user!, :only => [ :new, :edit, :show, :update, :create, :index ] 
+  skip_before_filter :require_no_authentication, only: :create
+  load_and_authorize_resource
+
   def index
     @users = User.all
-    respond_to do |format|
-      format.json { render :json => @users }
-      format.xml  { render :xml => @users }
-      format.html
-    end
   end
  
-  # GET /users/new
-  # GET /users/new.xml                                            
-  # GET /users/new.json                                    HTML AND AJAX
-  #-------------------------------------------------------------------
   def new
-    @user = User.new
-    respond_to do |format|
-      format.html # new.html.erb
-      format.json { render json: @user }
-    end
   end
- 
-  # GET /users/1
-  # GET /users/1.xml                                                       
-  # GET /users/1.json                                     HTML AND AJAX
-  #-------------------------------------------------------------------
+
   def show
-    @user = User.find(params[:id])
-    respond_to do |format|
-      format.json { render :json => @user }
-      format.xml  { render :xml => @user }
-      format.html      
-    end
- 
-  rescue ActiveRecord::RecordNotFound
-    respond_to_not_found(:json, :xml, :html)
   end
- 
-  # GET /users/1/edit                                                      
-  # GET /users/1/edit.xml                                                      
-  # GET /users/1/edit.json                                HTML AND AJAX
-  #-------------------------------------------------------------------
+
   def edit
-    @user = User.find(params[:id])
-    respond_to do |format|
-      format.html
-      format.json { render :json => @user }   
-      format.xml  { render :xml => @user }
-    end
- 
-  rescue ActiveRecord::RecordNotFound
-    respond_to_not_found(:json, :xml, :html)
   end
  
-  # DELETE /users/1     
-  # DELETE /users/1.xml
-  # DELETE /users/1.json                                  HTML AND AJAX
-  #-------------------------------------------------------------------
   def destroy
     @user = User.find(params[:id])
 
-    respond_to do |format|
-      if @user.save
-        flash[:notice] = "Account has been disabled"
-        format.html { redirect_to :action => :index }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+    if @user.destroy
+      flash[:notice] = "Account has been disabled"
+      redirect_to :action => :index
+    else
+      render action: "edit" 
     end
- 
-  rescue ActiveRecord::RecordNotFound
-    respond_to_not_found(:json, :xml, :html)
   end
- 
-  # POST /users
-  # POST /users.xml         
-  # POST /users.json                                      HTML AND AJAX
-  #-----------------------------------------------------------------
+
   def create
     @user = User.new(user_params)
 
-    account_params = user_params
-    if account_params[:password].blank?
-      account_params.delete("password")
-      account_params.delete("password_confirmation")
-    end
-
     if @user.save
-      respond_to do |format|
-        format.html { redirect_to admin_user_path(@user), notice: 'user was successfully created.' }
-        format.json { render json: admin_user_path(@user), status: :created, location: @user }
-      end
+      flash[:notice] = "User created."
+      redirect_to action: :index
     else
-      respond_to do |format|
-        format.html { render action: "new" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
+      render action: "new"
     end
   end
 
   def update
     if params[:user][:password].blank?
-      [:password,:password_confirmation,:current_password].collect{|p| params[:user].delete(p) }
+      [ :password, :password_confirmation ].collect{|p| params[:user].delete(p) }
+    end
+
+    if @user.update(user_params)
+      if @user == current_user
+        flash[:notice] = "Your account has been updated."
+        redirect_to root_path
+      else
+        flash[:notice] = "User updated."
+        redirect_to action: :index
+      end
     else
-      @user.errors[:base] << "The password you entered is incorrect" unless @user.valid_password?(params[:user][:current_password])
-    end
-    # required for settings form to submit when password is left blank
-    account_update_params = params[:user]
-    if account_update_params[:password].blank?
-      account_update_params.delete("password")
-      account_update_params.delete("password_confirmation")
-    end
-
-
-    if @user.update_attributes(user_params)  
-      redirect_to( users_path, notice: "Your account has been updated")
-    else      
-      render action: :edit, alert: 'There were some problems updating this user.'
-    end
-  end
-
-  # Get roles accessible by the current user
-  #----------------------------------------------------
-  def accessible_roles
-    @accessible_roles = Role.accessible_by(current_ability,:read)
-  end
- 
-  # Make the current user object available to views
-  #----------------------------------------
-  def get_user
-    @current_user = current_user
-  end
-
-  def get_particular_user    
-    if params[:id]
-      @user = User.find(params[:id])
+      render action: :edit
     end
   end
 
   private
 
-  def verify_is_admin
-    (current_user.nil?) ? redirect_to(root_path) : (redirect_to(root_path) unless current_user.admin?)
-  end
-
   def user_params
-    params.require(:user).permit(:id, :name, :email, :password, :password_confirmation, :roles)
+    permitted_params = [ :name, :email, :phone_number, :password, :password_confirmation ]
+    permitted_params << :roles if can? :manage, User
+    params.require(:user).permit(permitted_params)
   end
+  
 end
