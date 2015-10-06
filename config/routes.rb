a6deceaf02c2d2f4621fd6db39d5e5f8b0650613
Andy Hull <andyhull3@gmail.com>
2015-10-06 16:19:57 -0400

    added mobile teams to modal.

--- config/routes.rb
+++ config/routes.rb
@@ -29,6 +29,7 @@ Rails.application.routes.draw do
   resources :operation_periods, only: [ :update, :destroy ] do
     resources :clones, only: :create
     resources :first_aid_stations, only: [ :new, :create ]
+    resources :mobile_teams, only: [ :new, :create ]
   end
   resources :providers
   resources :provider_confirmations
