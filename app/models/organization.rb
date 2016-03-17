f20a936537267bf9ed32bc3485b00e0776a9b6d7
Andy Hull <andyhull3@gmail.com>
2016-03-17 10:26:50 -0400

    added NullOrganization.

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -32,5 +32,22 @@ class Organization < ActiveRecord::Base
       end
     end
   end
+end
 
+class NullOrganization
+  def name
+    'N/A'
+  end
+
+  def email
+    'zombie@fake.com'
+  end
+
+  def phone_number
+    '9999999999'
+  end
+
+  def address
+    '123 fake st'
+  end
 end
--- app/models/organization_type.rb
+++ app/models/organization_type.rb
@@ -3,6 +3,25 @@ class OrganizationType < ActiveRecord::Base
 
   def self.find_organizations(name)
     type = OrganizationType.find_by_name(name)
-    type.present? ? Organization.all.where(organization_type_id: type.id) : 'No Organizations'
+    type.present? ? organizations = Organization.all.where(organization_type_id: type.id) : organizations =[ NullOrganization.new]
+    organizations.empty? ? organizations =[ NullOrganization.new] : organizations
+  end
+
+  def self.find_permitters
+    type = OrganizationType.find_by_name("Event Permitter")
+    type.present? ? organizations = Organization.all.where(organization_type_id: type.id) : organizations =[ NullOrganization.new]
+    organizations.empty? ? organizations =[ NullOrganization.new] : organizations
+  end
+  
+  def self.find_promoters
+    type = OrganizationType.find_by_name("Event Producer")
+    type.present? ? organizations = Organization.all.where(organization_type_id: type.id) : organizations =[ NullOrganization.new]
+    organizations.empty? ? organizations =[ NullOrganization.new] : organizations
+  end
+
+  def self.find_providers
+    type = OrganizationType.find_by_name("EMS Provider")
+    type.present? ? organizations = Organization.all.where(organization_type_id: type.id) : organizations =[ NullOrganization.new]
+    organizations.empty? ? organizations =[ NullOrganization.new] : organizations
   end
 end
