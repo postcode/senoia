2ade9c643684a99e1b1ed1adb3b6a1cd7bacdeaa
Andy Hull <andyhull3@gmail.com>
2016-03-28 17:30:18 -0400

    updated default email domain.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => 'sfdemmedicalplans.com' }
+  config.action_mailer.default_url_options = { :host => ENV['DEVISE_EMAIL_HOST'] }
 end
