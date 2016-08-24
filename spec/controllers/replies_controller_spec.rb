4e97a5a634e2d9f1987668b0cdac95652f50a02c
Andy Hull <andyhull3@gmail.com>
2016-08-24 10:04:43 -0400

    fixed replies test.

--- spec/controllers/replies_controller_spec.rb
+++ spec/controllers/replies_controller_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe RepliesController do
 
   let(:comment) { create(:comment) }
-  
+
   context "when logged in as admin" do
 
     login_admin
@@ -20,21 +20,6 @@ RSpec.describe RepliesController do
         post :create, comment_id: comment.id, comment: { body: Faker::Lorem.paragraph }, format: "js"
         expect(comment.children).to include(assigns(:reply))
       end
-
-      let(:notification_recipient) { FactoryGirl.create(:user) }
-      let(:message_delivery) { instance_double(ActionMailer::MessageDelivery) }
-      
-      it "sends notifications" do
-
-        comment.commentable.users_who_can_view << notification_recipient
-        
-        expect(NotificationMailer).to receive(:notification)
-          .with({ notification: kind_of(Notification) })
-          .and_return(message_delivery)
-        expect(message_delivery).to receive(:deliver_later)
-
-        post :create, { "comment" => { "body" => Faker::Lorem.paragraph }, "element_id" => "contact_comment_text", "comment_id" => comment.id, "format" => "js" }
-      end
     end
-  end  
+  end
 end
