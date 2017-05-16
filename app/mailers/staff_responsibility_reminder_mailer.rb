db02f51f6b442ddf6826a9506c3dfb652925eadb
Andy Hull <andyhull3@gmail.com>
2017-05-16 09:38:58 -0400

    cc aram on email reminders

--- app/mailers/staff_responsibility_reminder_mailer.rb
+++ app/mailers/staff_responsibility_reminder_mailer.rb
@@ -4,6 +4,7 @@ class StaffResponsibilityReminderMailer < ActionMailer::Base
   def send_responsibility_reminder(plan)
     @plan = plan
     mail(to: plan.creator.email,
+         cc: "aram.bronston@sfgov.org",
          subject: "A Staff Responsibility list is required for #{@plan.name}")
   end
 end
