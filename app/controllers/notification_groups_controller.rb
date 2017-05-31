a71b691c08db558756a57cb829c9124a30f6fa46
Andy Hull <andyhull3@gmail.com>
2017-05-31 17:53:40 -0400

    added radio channel to dispatch. Added sorting to nofication group page.

--- app/controllers/notification_groups_controller.rb
+++ app/controllers/notification_groups_controller.rb
@@ -1,7 +1,9 @@
 class NotificationGroupsController < ApplicationController
   load_and_authorize_resource
+  helper_method :sort_column, :sort_direction
 
   def show
+    @users = User.includes(:notification_group_memberships).order("#{sort_column} #{sort_direction}")
   end
 
   def update
@@ -14,5 +16,21 @@ class NotificationGroupsController < ApplicationController
 
   def notification_group_params
     params.require(:notification_group).permit(notification_group_memberships_attributes: [:id, :user_id, :_destroy])
+  end
+
+  def sortable_columns
+    ["name", "email", "roles_mask", "notification_group_memberships.id"]
+  end
+
+  def sort_column
+    column = params[:column]
+    if params[:column] == "notification_group_memberships"
+      column = "notification_group_memberships.id"
+    end
+    sortable_columns.include?(column) ? column : "email"
+  end
+
+  def sort_direction
+    %w(asc desc).include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,15 @@
 module ApplicationHelper
-
   def form_builder_for(obj)
     simple_form_for obj do |f|
       return f
     end
   end
-  
+
+  def sort_link(column, title = nil)
+    title ||= column.titleize
+    direction = column == sort_column && sort_direction == "asc" ? "desc" : "asc"
+    icon = sort_direction == "asc" ? "glyphicon glyphicon-chevron-up" : "glyphicon glyphicon-chevron-down"
+    icon = column == sort_column ? icon : ""
+    link_to "#{title} <span class='#{icon}'></span>".html_safe, { column: column, direction: direction }
+  end
 end
